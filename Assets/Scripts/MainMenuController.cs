51485f97e405c44add126a46fd1cef4b859ec663
Jallow_加林 <bailo.jallow@yahoo.com>
2023-05-03 17:08:03 +0200

    test main menu ui button clicked

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    public UIDocument mainMenuUIDocument;
+    public VisualTreeAsset popupDialogTemplate;
+
+    private VisualElement root;
+
+    void Start()
+    {
+        root = mainMenuUIDocument.rootVisualElement;
+
+        // Connect button click events
+        root.Q<Button>("ARMode").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("Tours").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("LeaderBoard").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("Settings").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("Help").clicked += ShowNotImplementedPopup;
+    }
+
+    private void ShowNotImplementedPopup()
+    {
+        var popupDialog = popupDialogTemplate.CloneTree().Q("PopupDialog");
+        root.Add(popupDialog);
+
+        // Center the popup on the screen
+        popupDialog.style.left = (root.layout.width - popupDialog.layout.width) / 2;
+        popupDialog.style.top = (root.layout.height - popupDialog.layout.height) / 2;
+
+        // Connect close button click event
+        popupDialog.Q<Button>("CloseButton").clicked += () => {
+            root.Remove(popupDialog);
+        };
+    }
+}
