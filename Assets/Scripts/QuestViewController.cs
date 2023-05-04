406eef2172b2efa0003cb2eae99590e85d61165a
Ugnius27 <ugnx271@gmail.com>
2023-05-04 19:28:12 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Mo0nrak3r/KremsQuest""
    This reverts commit 8cff129c491ce0187544e3781ee64b3484935b95.

---
+++ Assets/Scripts/QuestViewController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class QuestViewController : MonoBehaviour
+{
+    public UIDocument questViewUIDocument;
+    public VisualTreeAsset popupDialogTemplate;
+
+    private VisualElement root;
+
+    void Start()
+    {
+        root = questViewUIDocument.rootVisualElement;
+
+        // Connect button click events
+        root.Q<Button>("BackButton").clicked += () => LoadTargetScene("MainMenuScene");
+        /*root.Q<Button>("QuestGames").clicked += () => LoadTargetScene("QuestViewScene");
+        root.Q<Button>("ARMode").clicked += () => LoadTargetScene("Geolocation");
+        //root.Q<Button>("ARMode").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("Tours").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("LeaderBoard").clicked += () => LoadTargetScene("LeaderboardScene");
+        root.Q<Button>("Settings").clicked += ShowNotImplementedPopup;
+        root.Q<Button>("Help").clicked += ShowNotImplementedPopup;*/
+    }
+
+    private void LoadTargetScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+   /* private void ShowNotImplementedPopup()
+    {
+        var popupDialog = popupDialogTemplate.CloneTree().Q("PopupDialog");
+        root.Add(popupDialog);
+
+        // Center the popup on the screen
+        popupDialog.style.left = (root.layout.width - popupDialog.layout.width) / 2;
+        popupDialog.style.top = (root.layout.height - popupDialog.layout.height) / 2;
+
+        // Connect close button click event
+        popupDialog.Q<Button>("CloseButton").clicked += () => {
+            root.Remove(popupDialog);
+        };
+    }*/
+}
